4aab6b8f19393ce84dc25c9b7a2a6e5c47a97af3
jamigo <jamigo@ya.ru>
2020-05-08 04:40:36 +0400

    Добавил функцию сборки посылок в зависимости от нажатой клавиши (команды) дата 30.04.20

--- RS-MON.py
+++ RS-MON.py
@@ -1,13 +1,57 @@
 from tkinter import *  
-from tkinter.ttk import *
+from tkinter.ttk import Combobox, Button, Radiobutton, Label, Entry, Spinbox
 from tkinter import Tk, Frame, Menu
 import serial
 import time
 
+#Переменные и константы
+Start = '55555555'
+Stop = '00'
+Number_plate = '00'
+
+k_CAN_inter = '01'
+v_interface_1 = '01'
+v_interface_2 = '02'
+v_interface_3 = '03'
+v_interface_4 = '04'
+v_interface_5 = '05'
+v_interface_6 = '06'
+
+k_Impuls_out = '02'
+v_Aplus = '01' 
+v_Aminus = '03'
+v_Rplus = '02'
+v_Rminus = '04'
+
+k_Interface = '19'
+v_CAN_ON = '00'
+v_RS485_ON = '01'
+
+k_Inter_counter = '1B'
+v_CAN_OFF = '00'
+v_CAN0_ON = '01'
+v_CAN1_ON = '02'
+
+Opros = '00'
+
+
+connected = 0
+data=0
+amount = 0
+stop = False
+flag = 0
+speeds = ['1200','2400', '4800', '9600', '19200', '38400', '57600', '115200']
+
+#Функция собирает посылку в зависимости от нажатых клавиш и выбора платы
+def Parcel(komanda, value):
+    Number_plate = hex(int(number_of_plate.get()))[2:]
+    parcel_full = Start + str(Number_plate).zfill(2) + komanda + value + Stop
+    return parcel_full
 
 #def counter_crc_funcion():
     
-def _onKeyRelease(event):   #Работа с буфером по клавишам Ctr-C, Ctr-V, Ctr-X
+#Работа с буфером по клавишам Ctr-C, Ctr-V, Ctr-X
+def _onKeyRelease(event):   
     ctrl = (event.state & 0x4) != 0
     if event.keycode==88 and ctrl and event.keysym.lower() != "x":
         event.widget.event_generate("<<Cut>>")
@@ -39,58 +83,34 @@ def serial_ports(): #Funcrion find all com-ports in Windows
             pass
     return result
 
-
-Aplus = '5555555500020100' 
-Aminus = '5555555500020300'
-Rplus = '5555555500020200'
-Rminus = '5555555500020400'
-CAN_ON = '5555555500190000'
-RS485_ON = '5555555500190100'
-CAN_OFF = '55555555001B0000'
-CAN0_ON = '55555555001B0100'
-CAN1_ON = '55555555001B0200'
-Opros = '5555555500000000'
-
-connected = 0
-data=0
-amount = 0
-stop = 0
-
-speeds = ['1200','2400', '4800', '9600', '19200', '38400', '57600', '115200']
-
-
-
 def serial_tx_cycle(): #Функция отправляет 1000 посылок, берет значения из поля тхт
-    ser = serial.Serial(combo.get(), combo1.get(), timeout = 1)
+    ser = serial.Serial(combo.get(), combo1.get(), timeout = 0.1)
     ser.write(controller_crc_function(txt.get()))
     lbl_parcel_tx = Label(window, text = txt.get()).place(x=200, y=290)
     serial_rx(ser)
+    print("Значение Стоп ",stop)
     if stop == False:
-        window.after(1000, serial_tx_cycle)
-    else:
-        ser.close(ser)
-        return
-
+        end = window.after(1000, serial_tx_cycle)
+            
+  
 def serial_tx(): #Функция отправляет заданное количество посылок, берет значения из поля тхт
-    ser = serial.Serial(combo.get(), combo1.get(), timeout = 1)
+    ser = serial.Serial(combo.get(), combo1.get(), timeout = 0.1)
     amount = int(number_of_parcel.get())
     parcel = str(txt.get())
     lbl_parcel_tx = Label(window, text = 00000000000000)
     lbl_parcel_tx = Label(window, text = txt.get()).place(x=200, y=290)
-    print(parcel)
     while amount != 0:
         ser.write(controller_crc_function(txt.get()))
         amount-=1
         serial_rx(ser)
 
-def serial_tx_code(parcel): #Функция отправляет заранее определенный код
-    ser = serial.Serial(combo.get(), combo1.get(), timeout = 1)
+def serial_tx_code(parcel_full): #Функция отправляет заранее определенный код
+    ser = serial.Serial(combo.get(), combo1.get(), timeout = 0.1)
     amount = int(number_of_parcel.get())
     lbl_parcel_tx = Label(window, text = 00000000000000)
-    lbl_parcel_tx = Label(window, text = parcel).place(x=200, y=290)
-    print(parcel)
+    lbl_parcel_tx = Label(window, text = parcel_full).place(x=200, y=290)
     while amount != 0:
-        ser.write(controller_crc_function(parcel))
+        ser.write(controller_crc_function(parcel_full))
         amount-=1
         serial_rx(ser)
     
@@ -100,12 +120,23 @@ def serial_rx(ser):
     parcel_hex = display_data_rx.hex()  #Переводим полученные данные в HEX-формат (убираем /x)
     parcel_hex = parcel_hex[18:]              #Удаляем отправленную посылку из принятых данных
     parcel_rx_up = parcel_hex.upper()   #Переводим все буквы в верхний регистр (для удобства)
-    #lbl_parcel_rx = Label(window, text = 00000000000000).place(x=200, y=320)
     lbl_parcel_rx = Label(window, text = parcel_rx_up).place(x=200, y=320) #Выводим в пользовательский интерфейс
     ser.close()
     
 def serial_stop(): #Функция отключает общение с COM-портом
-    stop = True
+    global flag
+    global stop
+    if flag != 2:
+        if stop == False:
+            stop = True
+            flag = 1
+            btn_stop_mark = Canvas(window, width=10, height=10, bg="red").place(x=320, y=245)
+        else:
+            stop = False
+            flag = 2
+            btn_stop_mark = Canvas(window, width=10, height=10, bg="green").place(x=320, y=245)
+    flag = 0
+        
    
 window = Tk()  #Main program
 window.title("Программа для проверки доработок Incotex")  
@@ -133,15 +164,13 @@ rad_wocrc.pack(side=LEFT)
 sel_CRC.set(1)
 
 lbl0 = Label(window, text = "Выберите COM-порт:").place(x=15, y=100)
-combo = Combobox(window)
+combo = Combobox(window, values = serial_ports() )
 combo.place(x=15, y=120)
-combo['values'] = (serial_ports())
 combo.current(0)
 
 lbl01 = Label(window, text = "Выберите скорость:").place(x=200, y=100)
-combo1 = Combobox(window)
+combo1 = Combobox(window, values = speeds)
 combo1.place(x=200, y=120)
-combo1['values'] = (speeds)
 combo1.current(3)
 
 lbl2 = Label(window, text = "Отправка произвольной посылки:").place(x=15, y=150)
@@ -153,12 +182,15 @@ lbl_number_of_parcel = Label(window, text="Кол-во повторов").place(
 
 number_of_plate = Spinbox(window, from_=0, to=256, width=5) #модуль для выбора количества посылок
 number_of_plate.place(x=220,y=180)
-number_of_plate.delete(0,"end")                            #удаление всех элементов из модуля, для установки значения по умолчанию
-number_of_plate.insert(0,0)                                #установка значения по умолчанию 1
+number_of_plate.delete(00,"end")                            #удаление всех элементов из модуля, для установки значения по умолчанию
+number_of_plate.insert(0,00)                                #установка значения по умолчанию 1
 lbl_number_of_plate = Label(window, text="Номер платы").place(x=310, y=180)
+
 
 txt = Entry(window, width=30)  # поле для ввода посылки
 txt.place(x=15, y=210) 
+txt.delete(0, "end")
+txt.insert(0, 5555555500000000)
 txt.focus()                   # при запуске программы фокусируемся в данном поле
 
 menu = Menu(tearoff=0) #пункты меню всплывающего в текстовом поле при нажатии правой клавиши
@@ -174,16 +206,26 @@ btn_stop = Button(window, text="Стоп", command = serial_stop).place(x=215, y
 lbl3 = Label(window, text = 'Отправленные данные:').place(x=15, y=290)
 lbl4 = Label(window, text = 'Принятые данные:').place(x=15, y=320)
 
-btn_Aplus = Button(window, text="А+ ", command = lambda: serial_tx_code(Aplus)).place(x=15, y=350)
-btn_Aminus = Button(window, text="А- ", command = lambda: serial_tx_code(Aminus)).place(x=15, y=380)
-btn_Rplus = Button(window, text="R+ ", command = lambda: serial_tx_code(Rplus)).place(x=15, y=410)
-btn_Rminus = Button(window, text="R- ", command = lambda: serial_tx_code(Rminus)).place(x=15, y=440)
-btn_CAN_ON = Button(window, text="CAN_ON ", command = lambda: serial_tx_code(CAN_ON)).place(x=150, y=350)
-btn_CAN_OFF = Button(window, text="CAN_OFF ", command = lambda: serial_tx_code(CAN_OFF)).place(x=150, y=380)
-btn_RS485_ON = Button(window, text="RS485_ON ", command = lambda: serial_tx_code(RS485_ON)).place(x=150, y=410)
-btn_CAN0_ON = Button(window, text="CAN0_ON ", command = lambda: serial_tx_code(CAN0_ON)).place(x=150, y=440)
-btn_CAN1_ON = Button(window, text="CAN1_ON ", command = lambda: serial_tx_code(CAN1_ON)).place(x=285, y=350)
-btn_opros = Button(window, text="Опросить ", command = lambda: serial_tx_code(Opros)).place(x=285, y=440)
+btn_Aplus = Button(window, text="А+ ", command = lambda: serial_tx_code(Parcel(k_Impuls_out,v_Aplus))).place(x=15, y=350)
+btn_Aminus = Button(window, text="А- ", command = lambda: serial_tx_code(Parcel(k_Impuls_out,v_Aminus))).place(x=15, y=380)
+btn_Rplus = Button(window, text="R+ ", command = lambda: serial_tx_code(Parcel(k_Impuls_out,v_Rplus))).place(x=15, y=410)
+btn_Rminus = Button(window, text="R- ", command = lambda: serial_tx_code(Parcel(k_Impuls_out,v_Rminus))).place(x=15, y=440)
+
+btn_CAN_ON = Button(window, text="CAN_ON ", command = lambda: serial_tx_code(Parcel(k_Interface, v_CAN_ON))).place(x=150, y=350)
+btn_RS485_ON = Button(window, text="RS485_ON ", command = lambda: serial_tx_code(Parcel(k_Interface, v_RS485_ON))).place(x=150, y=380)
+btn_opros = Button(window, text="Опросить ", command = lambda: serial_tx_code(Parcel(Opros,Opros))).place(x=150, y=440)
+
+btn_CAN_01 = Button(window, text="CAN1 ", command = lambda: serial_tx_code(Parcel(k_CAN_inter, v_interface_1)), width = 5).place(x=285, y=350)
+btn_CAN_02 = Button(window, text="CAN2 ", command = lambda: serial_tx_code(Parcel(k_CAN_inter, v_interface_2)), width = 5).place(x=285, y=380)
+btn_CAN_03 = Button(window, text="CAN3 ", command = lambda: serial_tx_code(Parcel(k_CAN_inter, v_interface_3)), width = 5).place(x=285, y=410)
+btn_CAN_04 = Button(window, text="CAN4 ", command = lambda: serial_tx_code(Parcel(k_CAN_inter, v_interface_4)), width = 5).place(x=285, y=440)
+btn_CAN_05 = Button(window, text="CAN5 ", command = lambda: serial_tx_code(Parcel(k_CAN_inter, v_interface_5)), width = 5).place(x=345, y=350)
+btn_CAN_06 = Button(window, text="CAN6 ", command = lambda: serial_tx_code(Parcel(k_CAN_inter, v_interface_6)), width = 5).place(x=345, y=380)
+
+btn_CAN_OFF = Button(window, text="CAN_All_OFF ", command = lambda: serial_tx_code(Parcel(k_Inter_counter, v_CAN_OFF))).place(x=420, y=350)
+btn_CAN0_ON = Button(window, text="CAN0_ON ", command = lambda: serial_tx_code(Parcel(k_Inter_counter, v_CAN0_ON))).place(x=420, y=380)
+btn_CAN1_ON = Button(window, text="CAN1_ON ", command = lambda: serial_tx_code(k_Inter_counter, v_CAN1_ON)).place(x=420, y=410)
+
 
 window.mainloop()
 
